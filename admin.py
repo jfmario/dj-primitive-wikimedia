1637fd655851878e7a1568f1b85482858e0dbb53
John Francis Marion <john.f.marion@gmail.com>
2016-10-12 22:57:09 +0000

    update

--- admin.py
+++ admin.py
@@ -1,3 +1,5 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from primitive_wikimedia.models import PrimitiveWikimediaEntry
+
+admin.site.register ( PrimitiveWikimediaEntry )
--- models.py
+++ models.py
@@ -1,3 +1,19 @@
+
 from django.db import models
 
 # Create your models here.
+class PrimitiveWikimediaEntry ( models.Model ):
+    
+    creation_date = models.DateTimeField ( auto_now_add=True )
+    orig_url = models.CharField ( max_length=128 )
+    url1 = models.CharField ( max_length=128 )
+    url2 = models.CharField ( max_length=128 )
+    url3 = models.CharField ( max_length=128 )
+    
+    class Meta:
+        ordering = ['-creation_date']
+        verbose_name = "Primitive Wikimedia Entry"
+        verbose_name_plural = "Primitive Wikimedia Entries"
+        
+    def __str__ ( self ):
+        return "[%s] Primitive Wikimedia Entry from %s" % ( self.creation_date.strftime ( '%Y%m%d' ), self.orig_url )
