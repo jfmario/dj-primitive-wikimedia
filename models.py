1650bae2c8a432148801f2c5600c4495e5d794c3
John Marion <John Marion>
2016-10-12 21:18:32 -0400

    uh oh

--- models.py
+++ models.py
@@ -1,3 +1,18 @@
+
 from django.db import models
 
-# Create your models here.
+class PrimitiveWikimediaEntry ( models.Model ):
+
+    creation_date = models.DateTimeField ( auto_now_add=True )
+    orig_url = models.CharField ( max_length=128 )
+    url1 = models.CharField ( max_length=128 )
+    url2 = models.CharField ( max_length=128 )
+    url3 = models.CharField ( max_length=128 )
+
+    class Meta:
+        ordering = ['-creation_date']
+        verbose_name = "Primitive Wikimedia Entry"
+        verbose_name_plural = "Primitive Wikimedia Entries"
+
+    def __str__ ( self ):
+        return self.creation_date.strftime ( "%B %-d, %Y" )
---
+++ settings.py
@@ -0,0 +1,18 @@
+
+FORMATS = [
+    {
+        'slug': 'combo50',
+        'subtitle': '50 Iterations',
+        'title': "Combo Mode"
+    },
+    {
+        'slug': 'tri800',
+        'subtitle': '800 Iterations',
+        'title': 'Triangle Mode'
+    },
+    {
+        'slug': 'cir1600',
+        'subtitle': "1600 Iterations",
+        'title': "Ellipse Mode"
+    }
+]
